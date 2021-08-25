6a453547e2b638b67ad64fc78b7ee7a558bf8f94
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-08-24 22:53:42 -0500

    hide Event(s) components

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Pages/Home/Home.js';
 import Calendar from './Pages/Calendar/CalendarApp.js';
 import Officers from './Pages/Officers/Officers.js'
-import EventsPage from './Pages/EventsPage/EventsPage.js';
+// import EventsPage from './Pages/EventsPage/EventsPage.js';
 import Resources from './Pages/Resources/Resources.js';
 import Footer from './Components/Footer/Footer.js';
-import SingleEventPage from './Pages/SingleEventPage/SingleEventPage.js';
+// import SingleEventPage from './Pages/SingleEventPage/SingleEventPage.js';
 import Archives from './Pages/Archives/Archives.js'
 import ScrollToTop from './Components/BackTop/BackTop.js';
 import Membership from './Pages/Membership/Membership';
@@ -24,8 +24,8 @@ function App() {
       <NavigationBar />
         <Switch>
           <Route exact path={['/', '/home']} component={Home} />
-          <Route path={`/event/:id`} exact strict component={SingleEventPage} />
-          <Route exact path='/events' exact strict component={EventsPage} />
+          {/* <Route path={`/event/:id`} exact strict component={SingleEventPage} /> */}
+          {/* <Route exact path='/events' exact strict component={EventsPage} /> */}
           <Route exact path='/calendar' component={Calendar} />
           <Route exact path='/officers' component={Officers} />
           <Route exact path='/resources' component={Resources} />
